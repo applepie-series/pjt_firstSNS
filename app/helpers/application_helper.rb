f1b1ea52121c10183a28c018d510fe31e5a521ce
hana_Y <applepie.series@gmail.com>
2019-12-22 16:28:23 +0900

    add application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ペ〜ジごとの完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = "insta"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
