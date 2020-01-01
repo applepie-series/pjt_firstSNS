3dfb1170154eba5167b360033dc73930e04f1db8
hana_Y <applepie.series@gmail.com>
2020-01-01 13:10:24 +0900

    add webpackage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'メールアドレス、もしくはパスワードが間違っています。'
       render 'new'
---
+++ app/javascript/jquery/sample.js
@@ -0,0 +1,6 @@
+
+<script type="text/javascript">
+$(document).ready(function(){
+$("h1").css("color","green")
+});
+</script>
