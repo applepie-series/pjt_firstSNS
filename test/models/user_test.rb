a1c8e9c4395fb120f65e987f6f3750bc995c524a
hana_Y <applepie.series@gmail.com>
2020-01-02 11:37:58 +0900

    add login cookie

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,4 @@
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
@@ -62,5 +63,9 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 7
     assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 end
