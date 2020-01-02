6fd4c5b2bcf20e0be5b1efb8f2b690499856422f
hana_Y <applepie.series@gmail.com>
2020-01-02 20:07:50 +0900

    Add mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,8 +32,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'http://127.0.0.1:3000' # ここをコピペすると失敗します。自分の環境に合わせてください。
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
