9afa3042416ef23f9f3a840b6c33322c71551667
hana_Y <applepie.series@gmail.com>
2020-03-07 09:53:07 +0900

    gem pg version 2.0

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,12 +20,6 @@ import './app/javascript/packs/pjt_firstSNS/app/assets/stylesheets/custom.scss'
 //= require bootstrap-sprockets
 //= require data-confirm-modal
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
 
 // require("trix")
 // require("@rails/actiontext")
