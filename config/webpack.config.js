05bfebd69e9b7759c9e8b5527c0f66b4a2eb0e96
hana_Y <applepie.series@gmail.com>
2020-03-07 13:32:16 +0900

    wabpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,25 +1,25 @@
-var webpack = require('webpack');
-var path = require('path');
-var fs = require('fs');
+// var webpack = require('webpack');
+// var path = require('path');
+// var fs = require('fs');
 
-var nodeModules = {};
-fs.readdirSync('node_modules')
-  .filter(function(x) {
-    return ['.bin'].indexOf(x) === -1;
-  })
-  .forEach(function(mod) {
-    nodeModules[mod] = 'commonjs ' + mod;
-  });
+// var nodeModules = {};
+// fs.readdirSync('node_modules')
+//   .filter(function(x) {
+//     return ['.bin'].indexOf(x) === -1;
+//   })
+//   .forEach(function(mod) {
+//     nodeModules[mod] = 'commonjs ' + mod;
+//   });
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'backend.js'
-  },
-  externals: nodeModules
+  // entry: './src/main.js',
+  // output: {
+  //   path: path.join(__dirname, 'build'),
+  //   filename: 'backend.js'
+  // },
+  // externals: nodeModules
   // 以下を追加した状態でも試してもエラーが発生
-  // resolve: {
-  //   modules: ['node_modules']
-  // }
+  resolve: {
+    modules: ['node_modules']
+  }
 }
